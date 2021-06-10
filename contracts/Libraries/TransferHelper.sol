4c43094bfbf0adb676091cdccd36b619e57935ef
Mike Calvanese <mfcalvanese@gmail.com>
2021-06-10 15:27:06 -0400

    add GPL 3 license

--- contracts/Libraries/TransferHelper.sol
+++ contracts/Libraries/TransferHelper.sol
@@ -1,6 +1,5 @@
-// SPDX-License-Identifier: MIT
-// https://github.com/Uniswap/uniswap-lib/blob/master/contracts/libraries/TransferHelper.sol
-pragma solidity ^0.7.0;
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.7.6;
 
 // helper methods for interacting with ERC20 tokens and sending ETH that do not consistently return true/false
 library TransferHelper {
