01dc0bd79aaac4054eaf35a716f5bd53df54e7ba
Kyle <the.big.kb123@gmail.com>
2021-09-21 17:52:12 -0400

    Audit Issue 29: Return on success

--- __snapshots__/LimitSwapVerifier.test.js
+++ __snapshots__/LimitSwapVerifier.test.js
@@ -1,5 +1,5 @@
-exports['LimitSwapVerifier tokenToToken() gas cost 1'] = 137145
+exports['LimitSwapVerifier tokenToToken() gas cost 1'] = 137151
 
-exports['LimitSwapVerifier ethToToken() gas cost 1'] = 119298
+exports['LimitSwapVerifier ethToToken() gas cost 1'] = 119304
 
-exports['LimitSwapVerifier tokenToEth gas cost 1'] = 111013
+exports['LimitSwapVerifier tokenToEth gas cost 1'] = 111019
